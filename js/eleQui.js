036e7a5c971eb5ae8bf0dc413bafbdccade68e08
viper091 <vitorperin.emanuel@gmail.com>
2017-09-11 14:10:18 -0300

    First

---
+++ js/eleQui.js
@@ -0,0 +1,463 @@
+
+var eleQui = {
+  "H": {
+    "nAtomico": "1",
+    "nome": "Hidrogênio"
+  },
+  "He": {
+    "nAtomico": "2",
+    "nome": "Hélio"
+  },
+  "Li": {
+    "nAtomico": "3",
+    "nome": "Lítio"
+  },
+  "Be": {
+    "nAtomico": "4",
+    "nome": "Berílio"
+  },
+  "B": {
+    "nAtomico": "5",
+    "nome": "Boro"
+  },
+  "C": {
+    "nAtomico": "6",
+    "nome": "Carbono"
+  },
+  "N": {
+    "nAtomico": "7",
+    "nome": "Nitrogênio"
+  },
+  "O": {
+    "nAtomico": "8",
+    "nome": "Oxigênio"
+  },
+  "F": {
+    "nAtomico": "9",
+    "nome": "Flúor"
+  },
+  "Ne": {
+    "nAtomico": "10",
+    "nome": "Neônio"
+  },
+  "Na": {
+    "nAtomico": "11",
+    "nome": "Sódio"
+  },
+  "Mg": {
+    "nAtomico": "12",
+    "nome": "Magnésio"
+  },
+  "Al": {
+    "nAtomico": "13",
+    "nome": "Alumínio"
+  },
+  "Si": {
+    "nAtomico": "14",
+    "nome": "Silício"
+  },
+  "P": {
+    "nAtomico": "15",
+    "nome": "Fósforo"
+  },
+  "S": {
+    "nAtomico": "16",
+    "nome": "Enxofre"
+  },
+  "Cl": {
+    "nAtomico": "17",
+    "nome": "Cloro"
+  },
+  "Ar": {
+    "nAtomico": "18",
+    "nome": "Argônio"
+  },
+  "K": {
+    "nAtomico": "19",
+    "nome": "Potássio"
+  },
+  "Ca": {
+    "nAtomico": "20",
+    "nome": "Cálcio"
+  },
+  "Sc": {
+    "nAtomico": "21",
+    "nome": "Escândio"
+  },
+  "Ti": {
+    "nAtomico": "22",
+    "nome": "Titânio"
+  },
+  "V": {
+    "nAtomico": "23",
+    "nome": "Vanádio"
+  },
+  "Cr": {
+    "nAtomico": "24",
+    "nome": "Cromo"
+  },
+  "Mn": {
+    "nAtomico": "25",
+    "nome": "Manganês"
+  },
+  "Fe": {
+    "nAtomico": "26",
+    "nome": "Ferro"
+  },
+  "Co": {
+    "nAtomico": "27",
+    "nome": "Cobalto"
+  },
+  "Ni": {
+    "nAtomico": "28",
+    "nome": "Níquel"
+  },
+  "Cu": {
+    "nAtomico": "29",
+    "nome": "Cobre"
+  },
+  "Zn": {
+    "nAtomico": "30",
+    "nome": "Zinco"
+  },
+  "Ga": {
+    "nAtomico": "31",
+    "nome": "Gálio"
+  },
+  "Ge": {
+    "nAtomico": "32",
+    "nome": "Germânio"
+  },
+  "As": {
+    "nAtomico": "33",
+    "nome": "Arsênio"
+  },
+  "Se": {
+    "nAtomico": "34",
+    "nome": "Selênio"
+  },
+  "Br": {
+    "nAtomico": "35",
+    "nome": "Bromo"
+  },
+  "Kr": {
+    "nAtomico": "36",
+    "nome": "Criptônio"
+  },
+  "Rb": {
+    "nAtomico": "37",
+    "nome": "Rubídio"
+  },
+  "Sr": {
+    "nAtomico": "38",
+    "nome": "Estrôncio"
+  },
+  "Y": {
+    "nAtomico": "39",
+    "nome": "Ítrio"
+  },
+  "Zr": {
+    "nAtomico": "40",
+    "nome": "Zircônio"
+  },
+  "Nb": {
+    "nAtomico": "41",
+    "nome": "Nióbio"
+  },
+  "Mo": {
+    "nAtomico": "42",
+    "nome": "Molibdênio"
+  },
+  "Tc": {
+    "nAtomico": "43",
+    "nome": "Tecnécio"
+  },
+  "Ru": {
+    "nAtomico": "44",
+    "nome": "Rutênio"
+  },
+  "Rh": {
+    "nAtomico": "45",
+    "nome": "Ródio"
+  },
+  "Pd": {
+    "nAtomico": "46",
+    "nome": "Paládio"
+  },
+  "Ag": {
+    "nAtomico": "47",
+    "nome": "Prata"
+  },
+  "Cd": {
+    "nAtomico": "48",
+    "nome": "Cádmio"
+  },
+  "In": {
+    "nAtomico": "49",
+    "nome": "Índio"
+  },
+  "Sn": {
+    "nAtomico": "50",
+    "nome": "Estanho"
+  },
+  "Sb": {
+    "nAtomico": "51",
+    "nome": "Antimônio"
+  },
+  "Te": {
+    "nAtomico": "52",
+    "nome": "Telúrio"
+  },
+  "I": {
+    "nAtomico": "53",
+    "nome": "Iodo"
+  },
+  "Xe": {
+    "nAtomico": "54",
+    "nome": "Xenônio"
+  },
+  "Cs": {
+    "nAtomico": "55",
+    "nome": "Césio"
+  },
+  "Ba": {
+    "nAtomico": "56",
+    "nome": "Bário"
+  },
+  "La": {
+    "nAtomico": "57",
+    "nome": "Lantânio"
+  },
+  "Ce": {
+    "nAtomico": "58",
+    "nome": "Cério"
+  },
+  "Pr": {
+    "nAtomico": "59",
+    "nome": "Praseodímio"
+  },
+  "Nd": {
+    "nAtomico": "60",
+    "nome": "Neodímio"
+  },
+  "Pm": {
+    "nAtomico": "61",
+    "nome": "Promécio"
+  },
+  "Sm": {
+    "nAtomico": "62",
+    "nome": "Samário"
+  },
+  "Eu": {
+    "nAtomico": "63",
+    "nome": "Európio"
+  },
+  "Gd": {
+    "nAtomico": "64",
+    "nome": "Gadolínio"
+  },
+  "Tb": {
+    "nAtomico": "65",
+    "nome": "Térbio"
+  },
+  "Dy": {
+    "nAtomico": "66",
+    "nome": "Disprósio"
+  },
+  "Ho": {
+    "nAtomico": "67",
+    "nome": "Hólmio"
+  },
+  "Er": {
+    "nAtomico": "68",
+    "nome": "Érbio"
+  },
+  "Tm": {
+    "nAtomico": "69",
+    "nome": "Túlio"
+  },
+  "Yb": {
+    "nAtomico": "70",
+    "nome": "Itérbio"
+  },
+  "Lu": {
+    "nAtomico": "71",
+    "nome": "Lutécio"
+  },
+  "Hf": {
+    "nAtomico": "72",
+    "nome": "Háfnio"
+  },
+  "Ta": {
+    "nAtomico": "73",
+    "nome": "Tântalo"
+  },
+  "W": {
+    "nAtomico": "74",
+    "nome": "Tungstênio"
+  },
+  "Re": {
+    "nAtomico": "75",
+    "nome": "Rênio"
+  },
+  "Os": {
+    "nAtomico": "76",
+    "nome": "Ósmio"
+  },
+  "Ir": {
+    "nAtomico": "77",
+    "nome": "Irídio"
+  },
+  "Pt": {
+    "nAtomico": "78",
+    "nome": "Platina"
+  },
+  "Au": {
+    "nAtomico": "79",
+    "nome": "Ouro"
+  },
+  "Hg": {
+    "nAtomico": "80",
+    "nome": "Mercúrio"
+  },
+  "Tl": {
+    "nAtomico": "81",
+    "nome": "Tálio"
+  },
+  "Pb": {
+    "nAtomico": "82",
+    "nome": "Chumbo"
+  },
+  "Bi": {
+    "nAtomico": "83",
+    "nome": "Bismuto"
+  },
+  "Po": {
+    "nAtomico": "84",
+    "nome": "Polônio"
+  },
+  "At": {
+    "nAtomico": "85",
+    "nome": "Astatínio"
+  },
+  "Rn": {
+    "nAtomico": "86",
+    "nome": "Radônio"
+  },
+  "Fr": {
+    "nAtomico": "87",
+    "nome": "Frâncio"
+  },
+  "Ra": {
+    "nAtomico": "88",
+    "nome": "Rádio"
+  },
+  "Ac": {
+    "nAtomico": "89",
+    "nome": "Actínio"
+  },
+  "Th": {
+    "nAtomico": "90",
+    "nome": "Tório"
+  },
+  "Pa": {
+    "nAtomico": "91",
+    "nome": "Protactínio"
+  },
+  "U": {
+    "nAtomico": "92",
+    "nome": "Urânio"
+  },
+  "Np": {
+    "nAtomico": "93",
+    "nome": "Netúnio"
+  },
+  "Pu": {
+    "nAtomico": "94",
+    "nome": "Plutônio"
+  },
+  "Am": {
+    "nAtomico": "95",
+    "nome": "Amerício"
+  },
+  "Cm": {
+    "nAtomico": "96",
+    "nome": "Cúrio"
+  },
+  "Bk": {
+    "nAtomico": "97",
+    "nome": "Berquélio"
+  },
+  "Cf": {
+    "nAtomico": "98",
+    "nome": "Califórnio"
+  },
+  "Es": {
+    "nAtomico": "99",
+    "nome": "Einstéinio"
+  },
+  "Fm": {
+    "nAtomico": "100",
+    "nome": "Férmio"
+  },
+  "Md": {
+    "nAtomico": "101",
+    "nome": "Mendelévio"
+  },
+  "No": {
+    "nAtomico": "102",
+    "nome": "Nobélio"
+  },
+  "Lr": {
+    "nAtomico": "103",
+    "nome": "Laurêncio"
+  },
+  "Rf": {
+    "nAtomico": "104",
+    "nome": "Rutherfórdio"
+  },
+  "Db": {
+    "nAtomico": "105",
+    "nome": "Dúbnio"
+  },
+  "Sg": {
+    "nAtomico": "106",
+    "nome": "Seabórgio"
+  },
+  "Bh": {
+    "nAtomico": "107",
+    "nome": "Bóhrio"
+  },
+  "Hs": {
+    "nAtomico": "108",
+    "nome": "Hássio"
+  },
+  "Mt": {
+    "nAtomico": "109",
+    "nome": "Meitnério"
+  },
+  "Ds": {
+    "nAtomico": "110",
+    "nome": "Darmstadtio"
+  },
+  "Rg": {
+    "nAtomico": "111",
+    "nome": "Ununúnio"
+  },
+  "Uub": {
+    "nAtomico": "112",
+    "nome": "Unúmbio"
+  },
+  "Uuq": {
+    "nAtomico": "114",
+    "nome": "Unilquádio"
+  },
+  "Uuh": {
+    "nAtomico": "116",
+    "nome": "Ununhexium"
+  },
+  "Uuo": {
+    "nAtomico": "118",
+    "nome": "Ununoctium."
+  }
+};
